44c1501e02bf341ccae3ba1993ebe18a179f57a5
Joannacierpka <joanna.cierpka@gmail.com>
2023-07-05 16:25:57 +0200

    lekcja 4 zadanie domowe

---
+++ zadanie domowe lvl 4/zadanie domowe lvl 4/Program.cs
@@ -0,0 +1,19 @@
+﻿var name = "Ewa";
+bool women = true;
+var age = 33;
+
+if(women == true && age < 30)
+{
+    Console.WriteLine("Nie rozumiem");
+}
+else
+{
+    if(name == "Ewa" && age == 33)
+    {
+        Console.WriteLine("Prawda");
+    }
+}
+if(women == false && age < 18)
+{
+    Console.WriteLine("Niepełnolerni mężczyzna");
+}
